1419829bbff33b444e1a06912ed77554e38b6b24
David <david.meth@hotmail.com>
2019-04-16 12:35:50 -0300

    one or other thing in welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,17 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    VI SOLUTIONS
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a >&nbsp;</a>
+                    <a >&nbsp;</a>
+                    <a >&nbsp;</a>
+                    <a >&nbsp;</a>
+                    <a >CADASTRO DE PRODUTOS</a>
+                    <a >&nbsp;</a>
+                    <a target="_blank" href="https://github.com/me42th"><sub>by</sub> <sup>me42th</sup></a>
                 </div>
             </div>
         </div>
